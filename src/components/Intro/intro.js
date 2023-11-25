a55b170a7a5cdbd8aa8f4da303d7e6329e0fdf41
Yilian90 <chenyilian90@gmail.com>
2023-11-25 20:34:08 +0800

    adjust font and size of the text

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -10,7 +10,7 @@ const Intro = () => {
         <div className="introContent">
             <span className="hello">Hello,</span>
             <span className="introText">I'm <span className="introName">Aty</span><br />Website Designer</span>
-            <p className="introPara">I am a skilled web designer with experience in creating visually appealing and user friendly websites.</p>
+            <p className="introPara">I am a skilled web designer with experience in creating <br /> visually appealing and user friendly websites.</p>
             <Link><button className="btn"><img src={btnImg} alt="Hire" className='btnImg' />Hire Me</button></Link>
         </div>
         <img src={bg} alt="Profile" className="bg" />
