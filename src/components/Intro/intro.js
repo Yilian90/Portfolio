7782fe68fd295334c10e5504baba80f317a85872
Yilian90 <chenyilian90@gmail.com>
2023-11-25 19:59:24 +0800

    add image and intro

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './intro.css';
 import bg from '../../assets/image.jpg';
+import btnImg from '../../assets/hireme.png'
+import { Link } from 'react-scroll'
 
 const Intro = () => {
   return (
     <section id="intro">
         <div className="introContent">
-            <span className="hello">Hello</span>
+            <span className="hello">Hello,</span>
             <span className="introText">I'm <span className="introName">Aty</span><br />Website Designer</span>
+            <p className="introPara">I am a skilled web designer with experience in creating visually appealing and user friendly websites.</p>
+            <Link><button className="btn"><img src={btnImg} alt="Hire" className='btnImg' />Hire Me</button></Link>
         </div>
         <img src={bg} alt="Profile" className="bg" />
     </section>
