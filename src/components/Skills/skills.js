33559883f7e52800110d9878236721fc19c82a2e
Yilian90 <chenyilian90@gmail.com>
2023-11-26 23:21:46 +0800

    update css file for skills

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import './skills.css'
-import UIDesign from '../../assets/ui-design.png'
-import WebDesign from '../../assets/website-design.png'
-import AppDesign from '../../assets/app-design.jpeg'
+import React from 'react';
+import './skills.css';
+import UIDesign from '../../assets/ui-design.png';
+import WebDesign from '../../assets/website-design.png';
+import AppDesign from '../../assets/app-design.jpeg';
 
 export const skills = () => {
   return (
