e4070bc4d59ec2981efedc9b63cebab851b01a94
Yilian90 <chenyilian90@gmail.com>
2023-11-29 13:52:49 +0800

    done contact section

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -3,12 +3,16 @@ import './contact.css'
 import Languages1 from '../../assets/css.png'
 import Languages2 from '../../assets/figma.png'
 import Languages3 from '../../assets/html.png'
-import Languages4 from '../../assets/react.png' 
+import Languages4 from '../../assets/react.png'
+import EmailIcon from '../../assets/emailIcon.png'
+import GithubIcon from '../../assets/githubIcon.png'
+import LinkedinIcon from '../../assets/linkedinIcon.png'
+
 const Contact = () => {
   return (
-    <section className='contactPage'>
+    <section id='contactPage'>
         <div id="clients">
-          <h1 className='contactPageTitle'>My </h1>
+          <h1 className='contactPageTitle'>My Programming Languages</h1>
           <p className='clientDesc'>
               I have experience  with these technologies
           </p>
@@ -20,7 +24,19 @@ const Contact = () => {
         </div>
         </div>
         <div id="contact">
-
+            <h1 className="contactPageTitle">Contact Me</h1>
+            <span className="contactDesc">Please fill out the form below to discuss any work opportunities.</span>
+            <form className="contactForm">
+              <input type="text" className="name" placeholder="Your Name" />
+              <input type="email" className="email" placeholder="Your Email" />
+              <textarea className ="msg" name="message" rows="5" placeholder="Your Message"></textarea> 
+              <button type="submit" value="send" className="submitBtn">Submit</button>
+              <div className="links">
+                  <img src={EmailIcon} alt="Email" className="link" />
+                  <img src={GithubIcon} alt="Github" className="link" />
+                  <img src={LinkedinIcon} alt="Linkedin" className="link" />
+              </div>
+            </form>
         </div>
     </section>
   )
