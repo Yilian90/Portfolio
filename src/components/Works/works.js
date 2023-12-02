597c561a08159029478465302a727ad97312e184
Yilian90 <chenyilian90@gmail.com>
2023-12-02 14:13:47 +0800

    add more picture for project

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './works.css'
 import Mobiklinik from '../../assets/mobiklinik.png'
-import Portfolio2 from '../../assets/portfolio-2.jpeg'
+import Cupcake from '../../assets/beauty-flower-cupcake.png'
 import Portfolio3 from '../../assets/portfolio-3.jpeg'
 import Portfolio4 from '../../assets/portfolio-4.jpeg'
 import Portfolio5 from '../../assets/portfolio-5.jpeg'
@@ -14,7 +14,9 @@ const Works = () => {
         <span className='worksDecs'>I take pride in paying attention to the smallest details and making sure that my works is pixel perferct. I am excited to bring my skills and experience to help businesses achieve their goals amd create a strong online presence.</span>
         <div className='worksImgs'>
             <img src={Mobiklinik} alt=""  className="worksImg" />
-            <img src={Portfolio2} alt="" className="worksImg" />
+            <p>download mobiklinik.apk here</p>
+            <img src={Cupcake} alt="" className="worksImg" />
+            <p>download mobiklinik.apk here</p>
             <img src={Portfolio3} alt="" className="worksImg" />
             <img src={Portfolio4} alt="" className="worksImg" />
             <img src={Portfolio5} alt="" className="worksImg" />
